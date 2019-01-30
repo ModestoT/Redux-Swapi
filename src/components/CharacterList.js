152dbfea53defc86da30f94126929d221536f049
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-01-30 16:11:21 -0600

    Placed the component did mount and state in the incorrect file. Moved them to the correct one. Everything is working as intended still. Added some CSS as well

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,39 +1,19 @@
 import React from "react";
-import { connect } from 'react-redux';
-
 import Character from "./Character";
 
-import { getChars } from '../actions';
 
-class CharacterList extends React.Component {
-  constructor(props){
-    super(props);
-  }
-  
-  componentDidMount(){
-    this.props.getChars();
-  }
+import './CharacterList.css';
 
-  render(){
+const CharacterList = props => {
     return (
       <ul>
-        {this.props.characters && (
-          this.props.characters.map(character => {
+        {props.characters.map(character => {
             return <Character key={character.name} character={character} />;
-          })
-        )}
+          })}
       </ul>
     );
-  };
 }
 
-const mapStateToProps = state => ({
-  characters: state.charsReducer.characters,
-  error: state.charsReducer.error,
-  fetchingChars: state.charsReducer.fetchingChars
-});
 
-export default connect(
-  mapStateToProps,
-  { getChars }
-)(CharacterList);
+
+export default CharacterList;
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -2,20 +2,19 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
+import { getChars } from '../actions';
 // import actions
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
-  }
 
-  componentDidMount() {
-    // call our action
+  componentDidMount(){
+    this.props.getChars();
   }
 
   render() {
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
+      return <div>Loading...</div>
     }
     return (
       <div className="CharactersList_wrapper">
@@ -27,9 +26,14 @@ class CharacterListView extends React.Component {
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
+
+const mapStateToProps = state => ({
+  characters: state.charsReducer.characters,
+  error: state.charsReducer.error,
+  fetchingChars: state.charsReducer.fetchingChars
+});
+
 export default connect(
-  null /* mapStateToProps replaces null here */,
-  {
-    /* action creators go here */
-  }
+  mapStateToProps,
+  { getChars }
 )(CharacterListView);
